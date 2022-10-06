5fb3146b86860f0efbd0d8568c62705f655ab72e
Osvak <61042986+Osvak@users.noreply.github.com>
2022-10-06 09:42:54 +0200

    scripts buenos (no los de mario)

--- Exercise2_Online/Assets/UDP.cs
+++ Exercise2_Online/Assets/UDP.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Socket : MonoBehaviour
+public class UDP : MonoBehaviour
 {
-
-    public bool UDP_TCP;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +15,4 @@ public class Socket : MonoBehaviour
     {
         
     }
-
-    
 }
