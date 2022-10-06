eeab2c88baffc013dc622069d8019fd7913d951e
Mario HerFer <41444696+MHF13@users.noreply.github.com>
2022-10-06 09:30:04 +0200

    First commit

---
+++ Exercise2_Online/Assets/Socket.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Socket : MonoBehaviour
+{
+
+    public bool UDP_TCP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
